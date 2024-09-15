97e7a3cee65ca38b5c50271c2c16c598b81e3f7c
Leonardo Hernandez <leonardo_hdz-halo@hotmail.com>
2024-09-14 20:45:22 -0600

    Base de Portafolio web

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    var typed = new Typed('#typed-text', {
+      strings: [
+        '&lt;p&gt;Leonardo Hez&lt;/p&gt;', // Versión con error ortográfico
+        '&lt;p&gt;Leonardo Hdz&lt;/p&gt;',
+        '&lt;p&gt;Programador&lt;/p&gt;',
+        '&lt;p&gt;Youtuber^-^&lt;/p&gt;',
+        '&lt;p&gt;Leonardo Hdz&lt;/p&gt;',  // Versión corregida
+      ],
+      typeSpeed: 100,
+      backSpeed: 60,
+      startDelay: 500,
+      backDelay: 1000,
+      loop: false,
+      showCursor: true,
+      cursorChar: '|',
+      cursorBlink: 530
+    });
+  });
